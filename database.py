e7c76e7354dc7783575c43ebab0c7776e2a3ec85
Alan George Trinidad Cruz <100612234+AlanCruz10@users.noreply.github.com>
2024-07-23 08:44:06 -0600

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # DataBase configuration
-DATABASE_URL = "mysql+pymysql://admin:Corazon666@database-1.cxo9hmgwb5h3.us-east-1.rds.amazonaws.com:3306/chats_db_langspeak"
+DATABASE_URL = "jdbc:mysql://admin:Corazon666@database-1.cxo9hmgwb5h3.us-east-1.rds.amazonaws.com:3306/chats_db_langspeak"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
